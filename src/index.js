abacc20db1196e867baf8687908ad1c8496b42dd
Marta2Ges <ggarridomarta@gmail.com>
2021-03-07 18:58:58 +0100

    add some style changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import '../src/stylesheets/App.scss';
 import App from './components/App';
 
 ReactDOM.render(
